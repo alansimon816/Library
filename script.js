cda76b0972d19e15a897f4fafac92363148dc43d
Alan <alansimon816@gmail.com>
2020-12-31 19:00:44 -0500

    Adds skeleton HTML, CSS, JS files.

---
+++ script.js
@@ -0,0 +1,37 @@
+let library = []
+
+// Adds a book to the library array 
+function addBooktoLibrary(book) {
+    library.push(book)    
+}
+
+// Stores the information associated with a book.
+function storeBookInfo() {
+
+}
+
+// Creates a div to be associated with a book.
+// Adds a data-attribute for index of book in library to the DOM element.
+function createBookDiv() {
+
+}
+
+// Returns the information associated with a book.
+function getBookInfo() {
+
+}
+
+// Removes a book from the library. 
+function removeBookFromLibrary() {
+
+}
+
+// Removes the div associated with the book.
+function removeBookiv() {
+
+}
+
+// Deletes stored information about the book.
+function deleteBookInfo() {
+    
+}
